d029506177353371e608429f6a2c3f5dca1b5246
Rawat11Uk01 <kashishrawat308@gmail.com>
2022-03-16 11:33:24 +0530

    first commit

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,35 +2,54 @@ import { useRef, useState,useEffect } from "react";
 
 
 const SimpleInput = (props) => {
-  const nameInputRef = useRef();
+ 
 
   const [enteredName, setEnteredName] = useState("");
-  const [enteredNameIsValid, setenteredNameIsValid]=useState(false);
+  // const [enteredNameIsValid, setenteredNameIsValid]=useState(false);
   const [enteredNameTouched, setEnteredNameTouched]= useState(false);
-  useEffect(()=>{
-    if(enteredNameIsValid){
-      console.log('yu hu')
-    }
-  },[enteredNameIsValid])
+
+  const enteredNameIsValid = enteredName.trim()!== '';
+  const nameInputISInvalid = !enteredNameIsValid && enteredNameTouched;
+  // useEffect(()=>{
+  //   if(enteredNameIsValid){
+  //     console.log('yu hu')
+  //   }
+  // },[enteredNameIsValid])
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
+    // if(event.target.value.trim()!== ''){
+    //   setenteredNameIsValid(true)
+      
+    // }
+  };
+  const nameInputBlurHandler = (event)=>{
+    setEnteredNameTouched(true);
+    // if(enteredName.trim()=== ''){
+    //   setenteredNameIsValid(false)
+      
+    // }
+
   };
   const formSubmissonHandler = (event) => {
     event.preventDefault();
-    setEnteredNameTouched(true);
-    if(enteredName.trim()=== ''){
-      setenteredNameIsValid(false)
+  
+   setEnteredNameTouched(true);
+    if(!enteredNameIsValid){
+      // setenteredNameIsValid(false)
       return;
     }
-    setenteredNameIsValid(true);
+
+    // setenteredNameIsValid(true);
     console.log(enteredName);
-    console.log(nameInputRef.current.value)
+
     setEnteredName('');
-   
+    setEnteredNameTouched(false);
+
 
   };
-  const nameInputISInvalid = !enteredNameIsValid && enteredNameTouched;
+ 
+
   const nameInputClasses = !nameInputISInvalid ? "form-control" : "form-control invalid";
   return (
     <form onSubmit={formSubmissonHandler}>
@@ -38,11 +57,12 @@ const SimpleInput = (props) => {
         <label htmlFor="name">Your Name</label>
         <input
       
-          ref={nameInputRef}
+      
           onChange={nameInputChangeHandler}
           type="text"
           id="name"
           value={enteredName}
+          onBlur={nameInputBlurHandler}
          
         />
          {nameInputISInvalid && <p className="error-text">name is not valid</p>}
