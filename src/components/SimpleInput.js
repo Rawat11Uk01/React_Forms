d187830cec4eee1c6a1f7be87a26f8f0fa5f046b
Rawat11Uk01 <kashishrawat308@gmail.com>
2022-03-16 12:31:34 +0530

    first commit

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,15 +1,20 @@
-import { useRef, useState,useEffect } from "react";
-
+import { useState } from "react";
 
 const SimpleInput = (props) => {
- 
-
   const [enteredName, setEnteredName] = useState("");
   // const [enteredNameIsValid, setenteredNameIsValid]=useState(false);
-  const [enteredNameTouched, setEnteredNameTouched]= useState(false);
-
-  const enteredNameIsValid = enteredName.trim()!== '';
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  // const [formIsValid, setFormIsValid] = useState(false);
+  const enteredNameIsValid = enteredName.trim() !== "";
   const nameInputISInvalid = !enteredNameIsValid && enteredNameTouched;
+  let formIsValid = false;
+
+  // useEffect(()=>{
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
+  // },[enteredNameIsValid]);
+
   // useEffect(()=>{
   //   if(enteredNameIsValid){
   //     console.log('yu hu')
@@ -18,24 +23,24 @@ const SimpleInput = (props) => {
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
+
     // if(event.target.value.trim()!== ''){
     //   setenteredNameIsValid(true)
-      
+
     // }
   };
-  const nameInputBlurHandler = (event)=>{
+  const nameInputBlurHandler = (event) => {
     setEnteredNameTouched(true);
     // if(enteredName.trim()=== ''){
     //   setenteredNameIsValid(false)
-      
-    // }
 
+    // }
   };
   const formSubmissonHandler = (event) => {
     event.preventDefault();
-  
-   setEnteredNameTouched(true);
-    if(!enteredNameIsValid){
+
+    setEnteredNameTouched(true);
+    if (!enteredNameIsValid) {
       // setenteredNameIsValid(false)
       return;
     }
@@ -43,32 +48,40 @@ const SimpleInput = (props) => {
     // setenteredNameIsValid(true);
     console.log(enteredName);
 
-    setEnteredName('');
+    setEnteredName("");
     setEnteredNameTouched(false);
-
-
   };
- 
 
-  const nameInputClasses = !nameInputISInvalid ? "form-control" : "form-control invalid";
+  const nameInputClasses = !nameInputISInvalid
+    ? "form-control"
+    : "form-control invalid";
   return (
     <form onSubmit={formSubmissonHandler}>
       <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
-      
-      
           onChange={nameInputChangeHandler}
           type="text"
           id="name"
           value={enteredName}
           onBlur={nameInputBlurHandler}
-         
         />
-         {nameInputISInvalid && <p className="error-text">name is not valid</p>}
+        {nameInputISInvalid && <p className="error-text">name is not valid</p>}
+      </div>
+
+      <div className={nameInputClasses}>
+        <label htmlFor="Email">Your Email</label>
+        <input
+          onChange={emailInputChangeHandler}
+          type="text"
+          id="Email"
+          value={enteredEmail}
+          onBlur={emailInputBlurHandler}
+        />
+        {nameInputISInvalid && <p className="error-text">name is not valid</p>}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
