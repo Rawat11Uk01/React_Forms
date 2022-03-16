99241bd38ead1051c3105cdddd2cde5acbc79aec
Rawat11Uk01 <kashishrawat308@gmail.com>
2022-03-16 16:06:28 +0530

    first commit

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
 const BasicForm = (props) => {
+const [firstName, setFirstName]= useState('');
+const firstNameChangeHandler = ((event)=>{
+  setFirstName(event.target.value);
+})
+
+
+
   return (
     <form>
       <div className='control-group'>
         <div className='form-control'>
           <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+          <input type='text' id='name' onChange={firstNameChangeHandler} />
         </div>
         <div className='form-control'>
           <label htmlFor='name'>Last Name</label>
--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+
 
 import useInput from '../hooks/use-input';
 
